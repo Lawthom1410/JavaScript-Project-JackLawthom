3f8fe609fbd140b6fc4cee847968eecb08aaa321
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-08-04 18:12:59 +0100

    Added link to comptrak slides

--- Home.js
+++ Home.js
@@ -1,3 +1,7 @@
 function comptrak() {
     window.location = "CompTrak/Login/Login.html";
 }
+
+function comptrakPresentation() {
+    window.location.href = "https://docs.google.com/presentation/d/1x1LCUDU5GT9P1XOsPQrl2tO_ilhvt25GRmobt9yCel8/edit?usp=sharing";
+}
